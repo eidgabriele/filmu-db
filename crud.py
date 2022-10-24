bf52a2243b6d818abcda07e0f20731159266351e
eid_gabriele <eid.gabriele@gmail.com>
2022-10-24 11:14:51 +0300

    crud functions - delete. update objects

--- crud.py
+++ crud.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import sessionmaker
 from model import engine, Movie, Director, Genre, Studio, Movie_genre, Movie_studio
+from pprint import pprint
 
 session = sessionmaker(bind=engine)()
 
@@ -13,9 +14,38 @@ def read_object(object_class):
     obj = session.query(object_class).all()
     return obj
 
+def delete_object(object_class, object_id):
+    obj = session.query(object_class).get(object_id)
+    if obj:
+        session.delete(obj)
+        session.commit()
+        return True
+    else:
+        print(f"Error: {object_class.__name__} object ID:{object_id} not found")
+
+def update_object(object_class, object_id, **kwargs):
+    obj = session.query(object_class).get(object_id)
+    if obj and kwargs:
+        for column_name, value in kwargs.items():
+            if hasattr(obj, column_name):
+                setattr(obj, column_name, value)
+            else:
+                print(f"Error: {obj} has no {column_name} attribute")
+        else:
+            session.commit()
+            return obj
+    else:
+        print(f"Error: {object_class.__name__} object ID:{object_id} not found")
+
 if __name__ == "__main__":
-    create_object(Movie, name = "Whiplash", release_year =2014, budget = 3300000,  runtime = 106, rating  = 8.5, director_id = 1)
+    # create_object(Movie, name = "The Thin Red Line", release_year =1998, budget = 52000000 , runtime =170, rating = 7.6, director_id = 3)
     # zanras = create_object(Genre, name="comedy")
-    # studija = create_object(Studio, name = "Paramount Pictures")
+    # studija = create_object(Studio, name = "With Fox 2000 Pictures", country = "USA")
+    # dir = create_object(Director, name = "Terrence ", surname = "Malick")
     print(read_object(Studio))
-    print(read_object(Movie))
+    # pprint(read_object(Movie))
+    # print(read_object(Director))
+    # delete_object(Director, 3)
+    # print(read_object(Director))
+    update_object(Studio, 2, country = "USA")
+    print(read_object(Studio))
