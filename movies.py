5eb5ce38618d4f06eba002859f3ff3c06ec82046
eid_gabriele <eid.gabriele@gmail.com>
2022-10-24 14:42:32 +0300

    view and deletion of records from terminal

--- movies.py
+++ movies.py
@@ -4,6 +4,26 @@ from crud import *
 
 session = sessionmaker(engine)()
 
+def choose_crud(object_class):
+    print(f"You have selected {object_class.__name__}")
+    print(f"Available actions:\n1 - view records,\n2 - add a new record,\n3 - delete record,\n4 - update record")
+    selection = str(input("Choose next step: "))
+    if selection == "1":
+        pprint(read_object(object_class))
+    elif selection == "2":
+        pass
+    elif selection == "3":
+        pprint(read_object(object_class))
+        record_id = int(input("Please select record number for deletion:"))
+        if record_id:
+            delete_object(object_class, record_id)
+            print(f"Record id {record_id} was successfully removed from database")
+    elif selection == "4":
+        pass
+    else:
+        print("Unavailabe option, try again.")
+
+
 while True:
     print(f"--- Movie database ---")
     print(f"1 - Movies")
@@ -12,7 +32,7 @@ while True:
     print(f"5 - Exit application")
     selection = str(input("Choose an option: "))
     if selection == "1":
-        pass
+        choose_crud(Movie)
     elif selection == "2":
         pass
     elif selection == "3":
